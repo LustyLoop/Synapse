ff45ac8b0c49a8acf6b2a2821ec651b1f46225ab
Arthur <smkttt90@gmail.com>
2026-08-29 11:29:48 +0300

    added work with API

---
+++ app/src/main/java/com/example/synapseapp/airequests/api/API.kt
@@ -0,0 +1,10 @@
+package com.example.synapseapp.airequests.api
+
+import com.example.synapseapp.BuildConfig
+
+
+object API {
+    const val API_KEY = BuildConfig.API_KEY
+    const val YANDEX_FOLDER_ID = BuildConfig.YANDEX_FOLDER_ID
+
+}
